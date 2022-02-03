d51e5e8b3dc320f16d763248d550e77b1eca6425
NAmin23 <NAmin23@uchicago.edu>
2022-02-03 00:59:57 -0500

    Created new Unity project for hand tracking

---
+++ HandTracker/Assets/Scripts/handTracker.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Runtime.InteropServices;
+
+public class handTracker : MonoBehaviour
+{
+  private string path; //Path of file
+  private float time; //Stores total playtime
+  private string coordinates;
+  private int i = 0;
+
+  public void CreateText()
+  {
+
+    path = Application.dataPath + "/" + "handTracker" + i + ".txt";
+
+    if (!File.Exists(path)) //Create new file if it doesn't exist
+    {
+      File.WriteAllText(path, "time\t" + "x\t" + "y\t" + "z\t");
+    }
+    else //If it does exist, create a new title for the data path
+    {
+      i++;
+      CreateText();
+    }
+  }
+
+  private void Awake()
+  {
+    CreateText();
+    coordinates = "";
+    time = .0f;
+    Time.timeScale = 0;
+  }
+
+  private void Update()
+  {
+    time += Time.deltaTime; //Increment time
+
+
+    if (Time.timeScale == 1)
+    {
+      coordinates = time.ToString() + "\t" + transform.position.x.ToString() + "\t" + transform.position.y.ToString() + "\t" + transform.position.z.ToString() + "\n";
+      File.AppendAllText(path, coordinates);
+    }
+  }
+}
