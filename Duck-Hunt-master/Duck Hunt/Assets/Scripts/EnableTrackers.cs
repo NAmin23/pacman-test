5ef64e5172647684c3548e269c49ac339afd0559
NAmin23 <57247003+NAmin23@users.noreply.github.com>
2022-04-01 14:53:50 -0500

    Kim data

---
+++ Duck-Hunt-master/Duck Hunt/Assets/Scripts/EnableTrackers.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableTrackers : MonoBehaviour
+{
+    public GameObject handTracker;
+    public GameObject elbowTracker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        handTracker.SetActive(true);
+        elbowTracker.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
